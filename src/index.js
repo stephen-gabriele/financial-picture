4bd2144a27029126042212df0bde19377a81263e
Stephen Gabriele <stephengabriele@Stephens-MBP.attlocal.net>
2022-04-21 02:41:13 -0400

    initial repo setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/main.css'
+
+const App = () => <div className='text-3xl underline leading-6 font-black'>do some ish</div>
+
+ReactDOM.render(<App />, document.getElementById('root'))
